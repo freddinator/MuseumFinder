35e8c957a6a9543f5f2138efd955e230e49a0122
Freddy Heppell <f.heppell@gmail.com>
2015-02-21 20:11:16 +0000

    Extracted top bar to separate file

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> Museum Finder</title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+	<div id="container">
+		<header>
+			<nav class="navbar navbar-default">
+  				<div class="container-fluid">
+    				<!-- Brand and toggle get grouped for better mobile display -->
+    				<div class="navbar-header">
+      					<a class="navbar-brand" href="#">Museum Finder</a>
+    				</div>
+  				</div> <!-- /.container-fluid -->
+			</nav>
+		</header>
--- verdict.php
+++ verdict.php
@@ -26,28 +26,10 @@ if ($natural == $science && $british == $va && $science == $british){
     $museums[] = $key;
   }
 }
+
+require_once 'header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title> Museum Finder</title>
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="stylesheet.css">
-</head>
-<body>
-	<div id="container">
-		<header>
-			<nav class="navbar navbar-default">
-  				<div class="container-fluid">
-    				<!-- Brand and toggle get grouped for better mobile display -->
-    				<div class="navbar-header">
-      					<a class="navbar-brand" href="#">Museum Finder</a>
-    				</div>
-  				</div> <!-- /.container-fluid -->
-			</nav>
-		</header>
 		<br>
 		<div class="text-center">
 <?php
